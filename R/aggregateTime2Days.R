11392c9a833a4a6c91cd694d580d8b1bb8dd0639
Mthrun <m.thrun@gmx.net>
2018-07-12 16:41:16 +0200

    .

---
+++ R/aggregateTime2Days.R
@@ -0,0 +1,22 @@
+aggregateTime2Days=function(Time,Data,fun,Header){
+  
+  if(missing(Header)){
+    if(is.matrix(Data)|is.data.frame(Data)){
+      if(length(Time)!=nrow(Data)) stop('Unequal number of rows in Data compared to Time')
+      Header=colnames(Data)
+    }else{
+    Header='Data'
+    if(length(Time)!=length(Data)) stop('Unequal length in Data compared to Time')
+    }
+  }
+
+  DF=data.frame(Time=Time,Data=Data)
+  DF$Time=cut(Time,breaks='days')
+  
+  dsummary = aggregate(DF$Data ~ DF$Time, FUN=fun,    data=DF)
+
+  colnames(dsummary)=c('Time',Header)
+
+  dsummary$Time=as.Date(dsummary$Time)
+  return(dsummary)
+}
--- R/setDiffTS.R
+++ R/setDiffTS.R
@@ -1,8 +1,8 @@
 setDiffTS=function(Time1,Time2,Vector1=NULL,Vector2=NULL){
   ind1=!Time1 %in% Time2
   ind2=!Time2 %in% Time1
-  TS1notinTS2 =  list(Time1[ind1],Vector1[ind])
-  TS2notinTS1 =  list(Time2[ind2],Vector2[ind])
+  TS1notinTS2 =  list(Time1[ind1],Vector1[ind2])
+  TS2notinTS1 =  list(Time2[ind2],Vector2[ind1])
   
   return(list(TS1notinTS2=TS1notinTS2,TS2notinTS1=TS2notinTS1))
 }
