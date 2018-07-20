6e6e39e4500d99ba6561a2d07ceec06ba4e5d745
Mthrun <m.thrun@gmx.net>
2018-07-20 15:44:20 +0200

    .

--- R/CrossCorr.R
+++ R/CrossCorr.R
@@ -1,14 +1,8 @@
-CrossCorr=function(TimeSeries1 , TimeSeries2 , nLags=2 ,PlotIt=FALSE,...){
+CrossCorr=function(TimeSeries1 , TimeSeries2 , nLags=2 ,type = "correlation",PlotIt=FALSE,main,...){
 #MT auch ccf in R, naeher vergleichen
 #CROSSCORR Compute or plot sample cross-correlation function.
 #   Compute or plot the sample cross-correlation function (XCF) between
-#   univariate, stochastic time series. When called with no output arguments,
-#   CROSSCORR displays the XCF sequence with confidence bounds.
-#
-#   [XCF, Lags, Bounds] = crosscorr(Series1 , Series2)
-#   [XCF, Lags, Bounds] = crosscorr(Series1 , Series2 , nLags , nSTDs)
-#
-#   Optional Inputs: nLags , nSTDs
+#   univariate, stochastic time series
 #
 # Inputs:
 #   Series1 - Vector of observations of the first univariate time series for
@@ -52,143 +46,43 @@ CrossCorr=function(TimeSeries1 , TimeSeries2 , nLags=2 ,PlotIt=FALSE,...){
 # See also AUTOCORR, PARCORR, FILTER.
 
   
-  res1=ccf(TimeSeries1, TimeSeries2, lag.max = nLags, type = "correlation",
-      plot = PlotIt, na.action = na.fail,...)
+  res1=ccf(TimeSeries1, TimeSeries2, lag.max = nLags, type = type,
+      plot = FALSE, na.action = na.fail,...)
+  ind=which.max(abs(res1$acf))
+  if(missing(main))
+    main=paste('Maximum of ccf(TS1,TS2) =',round(res1$acf[ind],2),'at lag =',ind-nLags)
+  
+  if(PlotIt){
+
+    def.par <-par(no.readonly = TRUE)# save default, for resetting...
+      m <-
+        graphics::layout(matrix(c(1, 1, 2, 2), 2, 2))
+
+    res1=ccf(TimeSeries1, TimeSeries2, lag.max = nLags, type = type,
+             plot = PlotIt,main=main,...)
+    
+   
+    #The lag k value returned by ccf(x, y) estimates the correlation between x[t+k] and y[t].
+    TSslagged=cbind(LagVector(as.numeric(TimeSeries1),ind-nLags),as.numeric(TimeSeries2))
+    TSslagged=TSslagged[complete.cases(TSslagged),]
+    
+    if(type == "correlation"){
+    plot(TSslagged[,1],TSslagged[,2],xlab=paste0('lag(TS1,',ind-nLags,')'),ylab='TS2',main=paste('Spearman Correlation =',round(cor(TSslagged[,1],TSslagged[,2],method = 'spearman'),2),'at lag =',ind-nLags))
+    }
+    if(type == "covariance"){
+      #requireNamespace('DataVisualizations')
+      #DataVisualizations::DualAxisLineChart(1:nrow(TSslagged),TSslagged[,1],TSslagged[,2])
+      plot(TSslagged[,1],type='l',col='black',xlab='Time',ylab=paste0('lag(TS1,',ind-nLags,')','(black) TS2(blue)'),main=paste('Spearman Covariance =',round(cov(TSslagged[,1],TSslagged[,2],method = 'spearman'),2),'at lag =',ind-nLags))
+      points(TSslagged[,2],type='l',col='blue')
+    }
+    par(def.par)
+   
+  }
+  bounds =  c(2,-2) / sqrt(length(TimeSeries1)) 
   return(invisible(res1))
   
   
   # res2=ccf(TimeSeries1, TimeSeries2, lag.max = nLags, type = "covariance",
   #     plot = PlotIt, na.action = na.fail)
-#   Copyright 1999-2003 The MathWorks, Inc.
-#   $Revision: 1.1 $  $Date: 2005/05/10 12:21:03 $
-# if nargin < 2
-#    error('GARCH:crosscorr:UnspecifiedInput' , ' ''Series1'' and ''Series2'' are both required.') 
-# end
-# 
-# 
-# [rows , columns]  =  size(Series1) 
-# 
-# if ((rows ~= 1) & (columns ~= 1)) | (rows*columns < 2)
-#    error('GARCH:crosscorr:NonVectorSeries1' , ' Input ''Series1'' must be a vector.') 
-# end
-# 
-# [rows , columns]  =  size(Series2) 
-# 
-# if ((rows ~= 1) & (columns ~= 1)) | (rows*columns < 2)
-#    error('GARCH:crosscorr:NonVectorSeries2' , ' Input ''Series2'' must be a vector.') 
-# else
-# 
-# end
-# 
-# rowSeries   =  size(Series1,1) == 1 
-# 
-# Series1     =  Series1(:)                               # Ensure a column vector
-# Series2     =  Series2(:)                               # Ensure a column vector
-# 
-# n           =  min(length(Series1) , length(Series2))   # Sample size.
-# defaultLags =  20                                       # BJR recommend about 20 lags.
-# 
-# #
-# # Ensure the number of lags, nLags, is a positive
-# # integer scalar and set default if necessary.
-# #
-# 
-# if (nargin >= 3) & ~isempty(nLags)
-#    if prod(size(nLags)) > 1
-#       error('GARCH:crosscorr:NonScalarLags' , ' Number of lags ''nLags'' must be a scalar.') 
-#    end
-#    if (round(nLags) ~= nLags) | (nLags <= 0)
-#       error('GARCH:crosscorr:NonPositiveInteger' , ' Number of lags ''nLags'' must be a positive integer.') 
-#    end
-#    if nLags > (n - 1)
-#       error('GARCH:crosscorr:InputTooLarge' , ' Number of lags ''nLags'' must not exceed ''Series'' length - 1.') 
-#    end
-# else
-#    nLags  =  min(defaultLags , n - 1) 
-# end
-# 
-# #
-# # Ensure the number of standard deviations, nSTDs, is a positive
-# # scalar and set default if necessary.
-# #
-# 
-# if (nargin >= 4) & ~isempty(nSTDs)
-#    if prod(size(nSTDs)) > 1
-#       error('GARCH:crosscorr:NonScalarSTDs' , ' Number of standard deviations ''nSTDs'' must be a scalar.') 
-#    end
-#    if nSTDs < 0
-#       error('GARCH:crosscorr:NegativeSTDs' , ' Number of standard deviations ''nSTDs'' must be non-negative.') 
-#    end
-# else
-#    nSTDs =  2      # Default is 2 standard errors (95# condfidence interval).
-# end
-# 
-# #
-# # The FILTER command could be used to compute the XCF, but FFT-based
-# # computation is significantly faster for large data sets.
-# #
-# 
-# Series1 =  Series1 - mean(Series1) 
-# Series2 =  Series2 - mean(Series2) 
-# L1      =  length(Series1) 
-# L2      =  length(Series2) 
-# 
-# if L1 > L2
-#    Series2(L1)  =  0 
-# elseif L1 < L2
-#    Series1(L2)  =  0 
-# end
-# 
-# nFFT   =  2^(nextpow2(max([L1 L2])) + 1) 
-# F      =  fft([Series1(:) Series2(:)] , nFFT) 
-# 
-# ACF1   =  ifft(F(:,1) .* conj(F(:,1))) 
-# ACF2   =  ifft(F(:,2) .* conj(F(:,2))) 
-# 
-# XCF    =  ifft(F(:,1) .* conj(F(:,2))) 
-# XCF    =  XCF([(nLags+1:-1:1)  (nFFT:-1:(nFFT-nLags+1))]) 
-# XCF    =  real(XCF) / (sqrt(ACF1(1)) * sqrt(ACF2(1))) 
-# 
-# Lags   =  [-nLags:nLags]' 
-# bounds =  [nSTDs   -nSTDs] / sqrt(n) 
-# 
-# 
-# if nargout == 0                     # Make plot if requested.
-# 
-# #
-# #  Plot the sample XCF.
-# #
-#    lineHandles  =  stem(Lags , XCF , 'filled' , 'r-o') 
-#    set   (lineHandles(1) , 'MarkerSize' , 4)
-#    grid  ('on')
-#    xlabel('Lag')
-#    ylabel('Sample Cross Correlation')
-#    title ('Sample Cross Correlation Function (XCF)')
-#    hold  ('on')
-# #
-# #  Plot the confidence bounds under the hypothesis
-# #  that the underlying series are uncorrelated.
-# #
-#    a  =  axis 
-# 
-#    plot([a(1) a(1)   a(2) a(2)] , [bounds([1 1]) bounds([2 2])] , '-b') 
-# 
-#    plot([a(1) a(2)] , [0 0] , '-k') 
-#    hold('off')
-# 
-# else
-# 
-# #
-# #  Re-format outputs for compatibility with the SERIES1 input. When SERIES1 is
-# #  input as a row vector, then pass the outputs as a row vectors  when SERIES1
-# #  is a column vector, then pass the outputs as a column vectors.
-# #
-# if(rowSeries){
-#       XCF     =  t(XCF) 
-#       Lags    =  t(Lags) 
-#       bounds  =  t(bounds) 
-# }
-
-#return(list(XCF=XCF , Lags=Lags , bounds=bounds))
 
 }
