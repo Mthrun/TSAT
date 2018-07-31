c20ba9c7923e9b3b7bc0589b7f885deea8d41476
Mthrun <m.thrun@gmx.net>
2018-07-31 11:06:30 +0200

    Linking GLM and Compount to Rathena

---
+++ R/GeneralizedLinearModels4TS.R
@@ -0,0 +1,5 @@
+GeneralizedLinearModels4TS=function(Response,SplitDataAt,Predictor1,Predictor2=NULL,CorrectionFactor=FALSE,PlotIt=TRUE,Time,Summary=FALSE,...){
+  
+  Rathena::GeneralizedLinearModels4TS=function(Response,SplitDataAt,Predictor1,Predictor2=NULL,CorrectionFactor=FALSE,PlotIt=TRUE,Time,Summary=FALSE,...)
+ 
+  }
---
+++ R/autoCompoundModel.R
@@ -0,0 +1,5 @@
+autoCompoundModel=function(DataFrame,TimeColumnName="Time",FeatureName="Absatz",SplitDataAt,Frequency='day',ForecastPeriods=10,Holidays=NULL,PlotIt=TRUE,xlab='Time',ylab='Feature',EquiDist=TRUE,MinLowerBound=NULL,MaxUpperBound=NULL,...){
+
+Rathena::autoCompoundModel(DataFrame,TimeColumnName="Time",FeatureName="Absatz",SplitDataAt,Frequency='day',ForecastPeriods=10,Holidays=NULL,PlotIt=TRUE,xlab='Time',ylab='Feature',EquiDist=TRUE,MinLowerBound=NULL,MaxUpperBound=NULL,...)
+
+}
