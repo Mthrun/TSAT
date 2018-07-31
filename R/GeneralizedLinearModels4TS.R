161a5677bb2cb021fe8279f74383bc1744709ac7
Mthrun <m.thrun@gmx.net>
2018-07-31 11:14:03 +0200

    .

--- R/GeneralizedLinearModels4TS.R
+++ R/GeneralizedLinearModels4TS.R
@@ -1,5 +1,5 @@
 GeneralizedLinearModels4TS=function(Response,SplitDataAt,Predictor1,Predictor2=NULL,CorrectionFactor=FALSE,PlotIt=TRUE,Time,Summary=FALSE,...){
   
-  Rathena::GeneralizedLinearModels4TS=function(Response,SplitDataAt,Predictor1,Predictor2=NULL,CorrectionFactor=FALSE,PlotIt=TRUE,Time,Summary=FALSE,...)
+  Rathena::GeneralizedLinearModels4TSv2(Response,SplitDataAt,Predictor1,Predictor2,CorrectionFactor,PlotIt,Time,Summary,...)
  
   }
--- R/autoCompoundModel.R
+++ R/autoCompoundModel.R
@@ -1,5 +1,5 @@
 autoCompoundModel=function(DataFrame,TimeColumnName="Time",FeatureName="Absatz",SplitDataAt,Frequency='day',ForecastPeriods=10,Holidays=NULL,PlotIt=TRUE,xlab='Time',ylab='Feature',EquiDist=TRUE,MinLowerBound=NULL,MaxUpperBound=NULL,...){
 
-Rathena::autoCompoundModel(DataFrame,TimeColumnName="Time",FeatureName="Absatz",SplitDataAt,Frequency='day',ForecastPeriods=10,Holidays=NULL,PlotIt=TRUE,xlab='Time',ylab='Feature',EquiDist=TRUE,MinLowerBound=NULL,MaxUpperBound=NULL,...)
+Rathena::autoCompoundModelv2(DataFrame,TimeColumnName,FeatureName,SplitDataAt,Frequency,ForecastPeriods,Holiday,PlotIt,xlab,ylab,EquiDist,MinLowerBound,MaxUpperBound,...)
 
 }
