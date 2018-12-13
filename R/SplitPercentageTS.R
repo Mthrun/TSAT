1b1050bfe4f5f22786bb504896bb5c9e07688041
Mthrun <m.thrun@gmx.net>
2018-12-13 11:07:43 +0100

    .

--- R/SplitPercentageTS.R
+++ R/SplitPercentageTS.R
@@ -1,40 +1,35 @@
-SplitPercentageTS=function(Datavector,percentage=80){
-  if(percentage>1) percentage=percentage/100
+SplitPercentageTS=function(Datavector,Time,Percentage=80){
   
-  N=length(Datavector)
-  if(percentage*N%%2!=0){
-    Datavector=head(Datavector,N-1)
-  }
-  N=length(Datavector)
-  
-  FactorA=1/(1/percentage-1)
-  
-  # FactorB=N-(N/FactorA)%%2
-  # 
-  z=1
-  for(i in 1:N){
-    if(z*FactorA<N)
-      z=z+1
+  N = length(Datavector)
+  Nt = NULL
+  if (!missing(Time)) {
+    Nt = length(Time)
+    if (N != Nt)
+      stop('Time vector is not of equal length to DataVector')
   }
   
-  # Split=round(N*percentage)
+  if (Percentage > 1)
+    Percentage = Percentage / 100
   
-  # ltrain=N-Split
-  # ltest=Split
-  # 
+  Split = ceiling(N * (1 - Percentage))
+  if (Split %% 2 != 0) {
+    Split = Split + 1
+  }
   
+  Train = head(Datavector, N - Split)
+  Test = tail(Datavector, Split)
+  if (!is.null(Nt)) {
+    TrainT = head(Time, N - Split)
+    TestT = tail(Time, Split)
+  } else{
+    TrainT = NULL
+    TestT = NULL
+  }
   
-  # if(N<100)
-  #   Split=Split-Split%%2
-  # else
-  #   Split=Split-Split%%16
-  # 
-  # Split/(N-Split) ->wholenumber
-  # 
-  # #The batch size must be evenly divisible into both the training an testing lengths (e.g. training length / batch size and testing 
-  # batch=
-    
-    
-  # Train=head(Datavector,N-Split)
-  # Test=tail(Datavector,Split)
+  return(list(
+    TrainingSet = Train,
+    TrainingTime = TrainT,
+    TestSet = Test,
+    TestTime = TestT
+  ))
 }
