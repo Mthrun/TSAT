b841c9c6cd84ca3d490de12e090be9397b847a42
Mthrun <m.thrun@gmx.net>
2019-01-07 16:48:09 +0100

    .

--- R/GetFullWeeksPerMonth.R
+++ R/GetFullWeeksPerMonth.R
@@ -1,4 +1,4 @@
-GetFullWeeksPerMonth=function(Time,DaysOrWeeks=TRUE){
+GetFullWeeksPerMonth=function(Time,DaysOrWeeks=TRUE,IgnoreWeekend=TRUE){
   
   requireNamespace('lubridate')
   
@@ -14,8 +14,16 @@ GetFullWeeksPerMonth=function(Time,DaysOrWeeks=TRUE){
   FullyInMonth=rep(FALSE,length(WeekTime))
   for(day in 1:length(WeekTime)){
     TimeCur=seq(from=WeekTime[day],by='1 days',length.out=7)
-    monthNoCur=lubridate::month(TimeCur)
-    if(length(unique(monthNoCur))==1) FullyInMonth[day]=TRUE
+
+    if(IgnoreWeekend){
+      monthNoCur=lubridate::month(TimeCur)
+      if(length(unique(monthNoCur))==1) FullyInMonth[day]=TRUE
+    }else{
+      tmp=lubridate::wday(TimeCur)
+      ind=which(tmp!=7&tmp!=1)
+      monthNoCur=lubridate::month(TimeCur)[ind]
+      if(length(unique(monthNoCur))==1) FullyInMonth[day]=TRUE
+    }
   }
   month=lubridate::month(WeekTime)
   month[!FullyInMonth]=0
