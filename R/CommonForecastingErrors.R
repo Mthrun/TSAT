32aed4079b928b633eba12c32a0d84e9957ab892
Mthrun <m.thrun@gmx.net>
2019-02-06 11:26:11 +0100

    rounding bugfix

--- R/CommonForecastingErrors.R
+++ R/CommonForecastingErrors.R
@@ -1,4 +1,4 @@
-CommonForecastingErrors=function(TestdataY,ForecastingF,epsilon=10^-4,na.rm=TRUE,Stepsize=1){
+CommonForecastingErrors=function(TestdataY,ForecastingF,epsilon=10^-4,na.rm=TRUE,stepsize=1,digits){
   if(isTRUE(na.rm)){ #achtung irgendwas stimmt hier nicht
     noNaNInd <- which(is.finite(TestdataY)&is.finite(ForecastingF))
     ForecastingF <- ForecastingF[noNaNInd]
@@ -14,7 +14,14 @@ CommonForecastingErrors=function(TestdataY,ForecastingF,epsilon=10^-4,na.rm=TRUE
   }else{
     TestdataYTMP=TestdataY
   }
-  
+  if(!missing(digits)){
+    exponent=abs(signif(log(epsilon,base = 10),1))
+    print(exponent)
+    if(digits>=exponent){
+      digits=exponent-1
+      warning('Rounding has to be to less digits than the exponent of epsilon. Reducing number of digits for rounding.')
+    }
+  }
 
   MAE=mean(AD,na.rm=na.rm)
 
@@ -32,15 +39,20 @@ CommonForecastingErrors=function(TestdataY,ForecastingF,epsilon=10^-4,na.rm=TRUE
     else
       TestdataYmase=TestdataY+runif(length(TestdataY),min = -u*epsilon,max = u*epsilon)
     
-    MASE=round(Metrics::mase(TestdataYmase,ForecastingF, step_size = Stepsize),2)
+    MASE=round(Metrics::mase(TestdataYmase,ForecastingF, step_size = stepsize),2)
   }else{
-    MASE=Metrics::mase(TestdataY,ForecastingF, step_size = Stepsize)
+    MASE=Metrics::mase(TestdataY,ForecastingF, step_size = stepsize)
   }
   requireNamespace('Rathena')
   Bias=Rathena::RootDeviance(TestdataY,ForecastingF)$bias
   MRD=Rathena::RootDeviance(TestdataY,ForecastingF)$MRD
   ForecastErrors=c(MAE,MAPE,smape,MASE,RMSE,Bias,MRD)
   names(ForecastErrors)=c('MAE','MAPE','SMAPE','MASE','RMSE','BIAS','MRD')
-  
+  if(!missing(digits)){
+    #Circumenvents Rs rule to round to even numbers in orter to represent an underlying continuous distribution
+    #this means 1.5 is now rounded to 2 and 2.5 to 3 instead that bot values are rounded to 2
+    ForecastErrors=round(ForecastErrors+epsilon*0.1,digits) 
+  }
+
   return(ForecastErrors)
   }
