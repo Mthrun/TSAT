ab62df5e5b72ea290a8a5c4b975bb267c4eee1ac
Mthrun <m.thrun@gmx.net>
2019-06-10 20:27:29 +0200

    bugifx FA

--- R/FourierAnalysis.R
+++ R/FourierAnalysis.R
@@ -1,19 +1,34 @@
-FourierAnalysis=function(Y,SamplingRate,na.rm = FALSE,PlotIt=FALSE){
+FourierAnalysis=function(Y,SamplingRate,AdjustWindow=FALSE,na.rm = FALSE,PlotIt=FALSE){
   N=length(Y)
-  Y=c(Y,rev(Y))
-  Y=c(Y,Y)
-  FFT=FastFourierTransformation(Y,na.rm = na.rm)
- 
-  if(missing(SamplingRate))
-    freq <- FFT$Frequencies
-  else
-    freq <- FFT$Frequencies*SamplingRate
-  PSD <- cbind(freq,FFT$Amplitude)
-  colnames(PSD)=c('Frequency','Amplitude')
-  PSD=PSD[-1,]
-  #PSD=PSD[-1,]
-  #nquist frequency
-  PSD=PSD[3:(floor(N/2)+1),]
+  if(isTRUE(AdjustWindow)){
+    Y=c(Y,rev(Y))
+  
+    Y=c(Y,Y)
+    N=length(Y)
+    FFT=FastFourierTransformation(Y,na.rm = na.rm)
+   
+    if(missing(SamplingRate))
+      freq <- FFT$Frequencies
+    else
+      freq <- FFT$Frequencies*SamplingRate
+    #print(freq[1:50])
+    PSD <- cbind(freq,FFT$Amplitude)
+    colnames(PSD)=c('Frequency','Amplitude')
+    #first is DC, second is window frequency
+    PSD=PSD[3:(floor(N/2)),]
+  }else{
+    FFT=FastFourierTransformation(Y,na.rm = na.rm)
+    
+    if(missing(SamplingRate))
+      freq <- FFT$Frequencies
+    else
+      freq <- FFT$Frequencies*SamplingRate
+    #print(freq[1:50])
+    PSD <- cbind(freq,FFT$Amplitude)
+    colnames(PSD)=c('Frequency','Amplitude')
+    #first is DC, second is window frequency
+    PSD=PSD[1:(floor(N/2)),]
+  }
   if(isTRUE(PlotIt)){
     obj=ggplot2::ggplot(data = as.data.frame(PSD), ggplot2::aes_string('Frequency','Amplitude')) + ggplot2::geom_bar(stat = "identity")+ggplot2::ggtitle('Frequency Domain')
   print(obj)
