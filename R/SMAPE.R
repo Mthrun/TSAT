1a6121976cf978044016b55f3d4d0bd2c4ca035a
Mthrun <m.thrun@gmx.net>
2020-02-29 16:33:07 +0100

    bugfix

--- R/SMAPE.R
+++ R/SMAPE.R
@@ -1,4 +1,4 @@
-SMAPE =function(X,Y,epsilon=10^-10,na.rm=FALSE,Silent=FALSE){
+SMAPE =function(X,Y,epsilon=10^-10,na.rm=FALSE,Silent=TRUE){
 # Symmetric mean absolute percentage error
 # Armstrong, J. S. (1985) Long-range Forecasting: From Crystal Ball to Computer, 2nd. ed. Wiley. ISBN 978-0-471-82260-8
 # Ultsch, A.: Is Log Ratio a Good Value for Measuring Return in Stock Investments? GfKl 2008, pp, 505-511, 2008.
@@ -9,22 +9,29 @@ SMAPE =function(X,Y,epsilon=10^-10,na.rm=FALSE,Silent=FALSE){
     X <- X[noNaNInd]
     Y <- Y[noNaNInd]
   }
-  if(isFALSE(Silent)){
+  if(!isTRUE(Silent)){
     if(length(X)!=length(Y)) stop('Length of X and Y do not match.')
     if(length(X)>1) return(sum(mapply(X,FUN = SMAPE,Y))/length(X))
   }
   oben=abs(Y-X)
   #idee: eigentlich muesste man negative und positive betraege getrennt aufsummieren und betrachten, vielleicht als wurzel und dann als komplexe zahl
   unten=abs(X)+abs(Y)
-  if(isFALSE(Silent)){
+  if(!isTRUE(Silent)){
     if(!is.finite(unten)) stop('Some of your values are not finite. Please use na.rm=TRUE')
   }
-  if(abs(unten)<epsilon){
-    if(isFALSE(Silent)){
+  
+  if(any(abs(unten)<epsilon)){
+    if(!isTRUE(Silent)){
       warning('X and Y are too small to calcualte Relative Differences. Returning 0')
     }
-    return(0)
+    ind=which(abs(unten)<epsilon)
+    Rel=100*oben/unten
+    Rel[ind]=0
+    return(Rel)
+  }else{
+    return(100*oben/unten)
   }
-  return(100*oben/unten)
+  
+
   #  smooth::SMAPE(actual, forecast, digits = 3) # does not use same formula...
 }
