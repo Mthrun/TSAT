f038af870584203c8bc5c98ce5bf172851250ba6
Mthrun <m.thrun@gmx.net>
2023-09-06 15:43:38 +0200

    now can be installed without internal dbt packages available

---
+++ R/ReadTS.R
@@ -0,0 +1,209 @@
+ReadTS = function(FileName,InDirectory=getwd(),ForceNumeric=FALSE){
+  # Load *.ts file
+  # V<- ReadTS(FileName,InDirectory)
+  # Series    = V$Series
+  # Rest     = V$FurtherTexts
+  # Comment  = V$Comments
+  #   
+  #
+  # INPUT
+  # FileName                        filename of *.names file
+  # OPTIONAL
+  # InDirectory                       InDirectory where *.names file is, default: current dir
+  # ForceNumeric              TRUE: saves numeric format of series, false: accepts text
+  # OUTPUT
+  # Series[1:d,1:2]                 matrix of Time and Seriesval, 1st column is Unix time or time in seconds, unique index from first column, Series contained in Column 2, without blanks
+  # FurtherTexts [1:d,1:x]          vector or matrix, All in colums 3 and beyond 
+  # Comments [1:ccc,]               vector or matrix,  string of all lines of Comments,  without the leading "#"
+  
+  #author: MCT 08/2023
+
+  if(grepl('.lrn',FileName)){
+    stop('Please use ReadLRN()')
+  }
+  
+  ################################################################################################################
+  ## Inputflow Kontrolle
+  ################################################################################################################
+  FileName  = addext(FileName,'ts')
+  checkFilename(FileName,Directory=InDirectory,Extension='ts',ReadOrWrite=TRUE,NameOfFunctionCalled='ReadTS()')
+
+  currentWD=getwd() #Aktuelles verzeichnis merken
+  setwd(InDirectory) #Ins Verzeichnis wo sich Datei befindet wechseln
+
+  ################################################################################################################
+    #Beginn Daten einzulesen
+    #Header einlesen mit HEADERSIZE
+  ################################################################################################################
+    
+  tryCatch({
+    
+      check=T
+      beginHeader = 1
+      while(check){
+        Header = readLines(FileName,n=beginHeader)[beginHeader]
+        if(gregexpr('%',Header)[1]==1){#Prozentzeichen muss als erstes Zeichen einer Zeile kommen
+          check=F
+        }else{
+          beginHeader = beginHeader+1 #Hier muss ich das dynamisch anpassen
+        }
+      }
+      if(beginHeader>1){
+        Comments=as.matrix(read.table(FileName, comment.char = "%", header=FALSE,  fill=TRUE, stringsAsFactors = FALSE, na.strings=c('NA','NaN'),nrows=beginHeader-1,blank.lines.skip=T))
+      }else{
+        Comments=NULL
+      }
+      if(!any(grepl('#',Comments))){
+        Comments=NULL
+      }
+      try(
+        if(!is.null(Comments)){
+          Comments=sub('# ','',Comments)
+          Comments=sub('#','',Comments)
+          Comments=sub('\t','',Comments)
+          Comments=paste(apply(Comments,1,paste,collapse=" "), collapse=" ")
+        }
+      )
+      # Wieso beginHeader+2 und nicht beginHeader+1?
+      # ORA/inst/extdata/.*.NAMES Beispiele zeigen, dass man +1 braucht
+      # da bei +2 die erste Datenzeile bereits mit ausgelesen wird
+      #HeaderLines=readLines(FileName,n=beginHeader+1) # Edit funktioniert an anderer Stelle dann wieder nicht ...
+      HeaderLines=readLines(FileName,n=beginHeader+2) # Original
+      
+      leng_size=sum(lengths(regmatches(HeaderLines, gregexpr("%", HeaderLines))))
+      ZahlPattern = "[0-9]+"
+      
+      if(leng_size==2){ # Edit July 2023, ORA NAMES examples didnt retrieved correct ncols
+        atomicVectorInd = regexpr(ZahlPattern, HeaderLines[beginHeader])
+        #atomicVecIdxCol = regexpr(ZahlPattern, HeaderLines[beginHeader+1])
+        tmpVar1 = unlist(strsplit(HeaderLines[beginHeader+1], "\t"))
+        tmpVar2 = gsub(" ", "", gsub(pattern = "[[:punct:]]", "", tmpVar1))
+        StartRow = atomicVectorInd[1]
+        StartCol = atomicVectorInd[2]
+        EndRow = StartRow+attributes(atomicVectorInd)$match.length[1]-1
+        EndCol = StartCol+attributes(atomicVectorInd)$match.length[2]-1
+        rows=as.numeric(substr(HeaderLines[beginHeader],StartRow,EndRow))
+        cols=as.numeric(substr(HeaderLines[beginHeader+1],StartCol,EndCol))
+        #cols=length(tmpVar2)
+      }else{
+        atomicVectorInd=regexpr(ZahlPattern, HeaderLines[beginHeader])
+        StartRow = atomicVectorInd
+        EndRow = StartRow+attributes(atomicVectorInd)$match.length-1 #
+        rows=as.numeric(substr(HeaderLines[beginHeader],StartRow,EndRow)) # wie viele datenzeilen gibt 
+        cols=NA
+      }
+      #Read Header
+      Line=HeaderLines[beginHeader+2]#
+        if(gregexpr('%',Line)[1]==1){#Prozentzeichen muss als erstes Zeichen einer Zeile kommen
+          
+        Line=sub('%','',Line)
+        
+        HeaderPatternStarts = "([[:alnum:]]|[[:punct:]])+"
+        HeaderInd=gregexpr(HeaderPatternStarts,Line )
+        Laengen = attributes(HeaderInd[[1]])$match.length
+        Starts =   unlist(HeaderInd)
+        Last = Starts+Laengen-1
+        Header=substring(Line,Starts,Last)
+        cols=length(Header)
+        }else{
+        #kein header da, legacy code
+          Header=NULL #untern abgefangen
+      }
+   },
+  error = function(c){
+    warning(c)
+    stop("Header or Comments are not reasonably defined, see Subversion/PUB/ZFileFormatDocuments for further instructions")
+  }
+  )
+
+  
+  
+  ################################################################################################################
+  ## Auslesen des Datensatzes
+  ################################################################################################################
+  #gibt fehler meldung aus wenn columns kleiner als 5 Datensätze sind.
+  #Grund ist eine C-Funktion die die ersten 5 Zeilen einliest um die Daten zubestimmen
+  #mehr infos: http://stackoverflow.com/questions/5990654/incomplete-final-line-warning-when-trying-to-read-a-csv-file-into-r
+  tryCatch({
+    Data = read.table(FileName,
+                      sep='\t',
+                      quote = "", #To disable quoting altogether
+                      comment.char = "%",   #comments are already read in, catch legacy names that has only one % and new names with two %
+                      header=FALSE,         #Wenn die erste Zeile der Header ist. zB. bei csv
+                      fill=T, 
+                      flush = T,
+                      stringsAsFactors = FALSE, 
+                      na.strings=c(''),
+                      skip=beginHeader)#,    #Die ersten HEADERSIZE zeilen werden nicht nochmal gelesen
+    ncols=ncol(Data)
+  if(!is.null(rows))
+    if(rows!=nrow(Data))
+      warning(paste0('ReadNAMES(): Number of rows ',nrow(Data),' does not equal number of rows in header ',rows))
+  
+  },
+  error = function(c){
+    print("Time, Series or Descriuption are not reasonably defined, see Subversion/PUB/ZFileFormatDocuments for further instructions")
+  }
+  )
+  
+  ################################################################################################################
+  ## Outputflow Kontrolle
+  ################################################################################################################
+  if(is.finite(cols)){
+    if(ncol(Data)!=cols){
+      warning(paste('rows of Time+Series+FurtherDescription(if exists)',ncol(Data),'does not equal number of columns stated in the header',cols))
+    }
+  }#otherwise old format, do nothin
+  KeyColumn=1
+  Time = Data[,KeyColumn]  #Umspeichern in Time 
+  
+  Data=Data[,-(KeyColumn)]  #Time als Spalte Loeschen
+  Data=data.frame(Data) #Dataframe mit einer spalte == liste -> muss wieder data.Frame sein
+  
+  #zurück wechseln ins alte Verzeichnis
+  setwd(currentWD)
+  
+  
+  # Series & FurtherTexts
+    NamesColumn = 2-KeyColumn
+    Series=Data[,NamesColumn]
+    if(isTRUE(ForceNumeric)){
+      Series=gsub("\\,","\\.",Series)
+      Series=gsub("[^0-9.]","",Series)
+      mode(Series)="numeric"
+    }else{
+      #Series=as.character(Series)
+    }
+ 
+  if(sum(!is.finite(suppressWarnings(as.numeric(Time))))==0)
+  	mode(Time)='numeric'
+  
+  if(ncols>2){
+    FurtherTexts = Data[,-NamesColumn]
+    FurtherTexts = as.matrix(FurtherTexts)# Edit July 2023: FurtherTexts kann Vektor sein
+    if(is.null(Header)){
+      Header=c("Series",paste0("C",1:ncol(FurtherTexts)))
+    }else{
+      Header=Header
+    }
+    HeaderFurtherText=Header[-1]#np neames
+    
+    if(length(HeaderFurtherText)==ncol(FurtherTexts))
+      colnames(FurtherTexts)=HeaderFurtherText#tail(Header[-1],ncol(FurtherText))#no key -> "-1
+
+    Series=cbind(Time,Series=Series)
+    colnames(Series)=Header[1:2]
+    colnames(FurtherTexts)=Header[3:length(Header)]
+    result=list(Series=Series,FurtherTexts=FurtherTexts,Header=Header,Comments=Comments)
+  }else{
+    if(is.null(Header)){
+      Header=c("Time","Series")
+    }else{
+    }
+    Series=cbind(Time,Series=Series)
+    colnames(Series)=Header[1:2]
+    result=list(Series=Series,Comments=Comments,Header=Header)
+  }
+
+  return (result)
+}
---
+++ R/WriteTS.R
@@ -0,0 +1,172 @@
+WriteTS=function(FileName,Time,SeriesVal,  FurtherTexts,Header, OutDirectory=getwd(),ForceNumeric=FALSE,
+                         Comments){
+  # WriteNAMES(FileName, Time, SeriesVal)
+  # Save series consisting of Time and SeriesVal and eventual FurtherTexts to a *.names file.
+  #
+  # INPUT
+  # FileName                  name of the  file to be written
+  # SeriesVal[1:n]                vector with text without blanks or numerical data to be put in each line
+  # Time[1:n]                  vector of row type, typically UnixTime or other time in seconds: unique key for each line, by default: [1:n]' 
+  
+  # OPTIONAL
+  # OutDirectory              the OutDirectory where to write into; if not given: current dir.
+  # ForceNumeric              TRUE: saves numeric format of SeriesVal, false: accepts text
+  # FurtherTexts[1:n,1:c]     string matrix with row FurtherTexts to be put in third column
+  # Header[1:(c+1),]   optional   header for the Nmes and FurtherTexts: Array of chars, this line wil be insterted at top of file with leading #
+  # Comments                  vector, Char Array or matrix, these lines will be insteted at top of file with leading #
+  #                           Note: Also allowed: Comments='first line \n# second line'
+  # 
+  #author: MCT 08/2023
+  
+  if(is.null(FileName)){
+    stop('WriteTS: FileName ist missing')
+  }
+  checkFilename(FileName, Directory = OutDirectory,        # Check FileName if correct extension, its existing, ...
+                Extension = 'ts', ReadOrWrite = FALSE,
+                NameOfFunctionCalled='WriteTS()')
+  
+  if(isTRUE(ForceNumeric)){
+    #if german notation
+    SeriesVal=gsub("\\,","\\.",SeriesVal)
+    SeriesVal=gsub("[^0-9.]","",SeriesVal)
+    mode(SeriesVal)="numeric"
+  }else{
+    #SeriesVal=as.character(SeriesVal)
+  }
+  
+  if(!is.null(SeriesVal)){      # Pruefung welche Inputvariablen angegeben wurden
+    rows=length(SeriesVal)
+    Data = data.frame(SeriesVal)   
+  }else{
+    stop('WriteTS: SeriesVal ist missing')
+  }
+
+  #----------------------------------------------------------------------------#
+  ## Abfang Fehler bezueglich Time
+  #----------------------------------------------------------------------------#
+  if(missing(Time)|is.null(Time)){#MT: Korrektur zur Timeerstellung
+    Time = 1:rows
+    warning('WriteTS: Time missing, generating Time as 1:n.')
+  }else{#Time set by user
+    if(length(Time) != rows){
+      stop('WriteTS():Length of Time isnt equal to the length of rows')
+    }
+    #convert to numeric so that format non scientific works
+    Time=as.numeric(Time)
+    if(sum(!is.finite(Time))>0){
+      warning('WriteTS(): Time has element that are not finite.')
+      Time[!is.finite(Time)]=NaN
+    }
+    #force no scientific format
+    Time=format(Time,scientific=FALSE)
+  }
+  
+  if(missing(FurtherTexts)) FurtherTexts=NULL
+  
+  if(missing(Header)){
+    Header=NULL #wird weiter unten generiert
+  }else{
+    # remove all Blanks from header
+    Header = gsub('  ','_',Header)
+    Header = sub(' ', '_', Header)
+    Header = gsub('\n','_',Header)
+    Header = gsub('\t','_',Header)
+  }       
+  
+  if(!is.null(FurtherTexts)){ 
+    if(!is.character(FurtherTexts)) stop("FurtherTexts must consist of Strings!")
+    #MT: Einfache Anfuerungsstriche sind ein sonderzeichen, welches eine korrekte
+    #    checksummen erstellen und ueberpruefung verhindert
+    FurtherTexts = sub("'","",sub("'","",FurtherTexts))
+    Data = cbind(Data,FurtherTexts)
+    if(is.null(Header)){
+      if(!is.null(colnames(FurtherTexts))){
+        Header=c("TimeInSeconds","Series",colnames(FurtherTexts))
+      }else{#=NULL wird weiter unten abgefangen
+        if(!is.null(ncol))
+          Header=Header=c("TimeInSeconds","Series",paste0("C",1:ncol(FurtherTexts)))
+      }
+    }
+  }else{
+    if(is.null(Header)) Header=c("TimeInSeconds","Series")
+  }
+  if((ncol(Data)+1)<length(Header)){
+    Header=Header[1:ncol(Data)]
+    warning("WriteTS(): Header is longer than SeriesVal plus furthertext (if exist) columns. Taking first elements.")
+  }
+  if((ncol(Data)+1)>length(Header)){
+    diff=ncol(Data)-length(Header)
+    Header=c(Header,paste0("C",1:diff))
+    warning("WriteTS(): Header is shorter than SeriesVal plus furthertext (if exist) columns. Adding elements.")
+  }
+  
+  if(ncol(Data) == 0 || nrow(Data) == 0 || typeof(Data) == 'NULL'){    # Zeilen anzahl bestimmen
+    stop('WriteTS(): No Data to write') #stop wenn Data NULL ist
+  }else{
+    rows    = nrow(Data)
+    columns = ncol(Data)
+  }
+  
+  FileName  = addext(FileName,'ts')    # richtige Datei endung
+  suppressWarnings((OutDirectory = normalizePath(OutDirectory))) 
+  currentWD = getwd()                     # Aktuelles verzeichnis merken
+  setwd(OutDirectory)                     # Ins Verzeichnis wo sich Datei befindet wechseln
+  
+  if(!is.null(Time)){
+    Timetmp = unique(Time)
+    if(length(Timetmp)!=length(Time)){
+      warning(paste0('WriteTS(): Time with length ',length(Time),' is not unique: ',length(Timetmp)))
+    }else{
+      Time=Timetmp
+    }
+  } #make Time unique
+  
+  Data = data.frame(Time,Data)    #Time hinzufuegen zu ColumnNames 
+  columns = columns + 1 #Wenn Time Hinzugefuegt wurde, muss auch die Spalten Anzahl angepasst werden
+  
+  #ColumnNames zu Data
+  #----------------------------------------------------------------------------#
+  # letzter Schritt ist die Klassen mit class() anzupassen:
+  # Problem war: das entweder nur string oder nur Zahl gefordert war
+  # R aber keine explizite Typen definition vorraus setzt. 
+  #----------------------------------------------------------------------------#
+  for(i in 1:columns){
+    if(is.integer(Data[,i])){
+      Data[,i]= as.numeric(Data[,i])
+    }
+    if(is.factor(Data[,i])){
+      Data[,i]=as.character(Data[,i])
+    }
+  }
+  #----------------------------------------------------------------------------#
+  ##---------------------Beginn mit schreiben in Datei--------------------------
+  #----------------------------------------------------------------------------#
+                                                                      # MCT way of WriteTS
+  if(!missing(Comments)){                                                        # Write comments if existing (1st row)
+    Comments = paste0('# ',Comments)                                             # One row starting with '#'
+    write.table(x = Comments, file = FileName, quote = FALSE, sep='\t',
+                row.names = FALSE, col.names = FALSE, na = 'NaN',append=FALSE)
+    furtherappend=TRUE
+  }else{
+    furtherappend=FALSE
+  }
+  
+  cols=ncol(Data)
+  # write dimensions Number of lines & columns
+  header = c(paste('%\t',rows),paste('%\t',cols))
+  write.table(header, FileName, quote=FALSE, row.names=FALSE, col.names=FALSE, na='NaN',append = furtherappend)
+
+  
+  cat('% ', file=FileName, append=TRUE)
+  cat(Header,'\n', file=FileName, append=TRUE, sep='\t')
+  
+  # Daten schreiben
+  write.table(Data, file = FileName, append = TRUE, quote = FALSE, sep='\t',
+              row.names = FALSE, col.names = FALSE, na = 'NaN')
+  #}
+  
+  ##------------Daten schreiben abgeschlossen-----------  
+  #zurueck wechseln ins alte Verzeichnis
+  setwd(currentWD)
+  #  }
+}
