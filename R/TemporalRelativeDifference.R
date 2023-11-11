87cd8982d60b47457fab5fa67df70a6cb5ad7afa
Mthrun <m.thrun@gmx.net>
2023-11-11 09:42:33 +0100

    refinement

--- R/TemporalRelativeDifference.R
+++ R/TemporalRelativeDifference.R
@@ -1,14 +1,16 @@
-TemporalRelativeDifference=function(Data,Lag=1,na.rm=FALSE,PlotIt=FALSE,Time,Silent=FALSE){
+TemporalRelativeDifference=function(Data,Time,Lag=1,na.rm=FALSE,PlotIt=FALSE,Silent=FALSE){
 #V=TemporalRelativeDifference(ElectricityBRD$Mrd_KWh,PlotIt=TRUE,Time=(ElectricityBRD$Time))
 #V=TemporalRelativeDifference(cbind(ElectricityBRD$Mrd_KWh,ElectricityBRD$Mrd_KWh))
 #Calculates the relative difference between positive Data and and lagged Data
 #INPUT
-# Data                numerical matrix of [1:n,1:d], d>=1
+# Data                numerical matrix of [1:n,1:d], d>=1; if vector named or matrix has rownames and Time input not given, names are transferred to he Reldiff output 
+#OPTIONAL
 # Lag                 The number of lags (in units of observations): back shifting: positiv value,forward shifting: negative value
 #                     Default is the difference to yesterday
+# Time                character vector 1:n defining the name, is only used to name the Reldiff output and for plotting
 # na.rm               FALSE; do nothing, TRUE: all nan to zero
-# PlotIt              TRUE: plots data
-# Silent              TRUE: not warnings or messages
+# PlotIt              If TRUE: plots data, works only for d=1, otherwise it is internally set to FALSE
+# Silent              If TRUE: not warnings or messages
 #OUTPUT
 # Reldiff             numerical matrix of [1:n,1:d], Negative value indicate that today is lower than yesterday 
 #                     and positive values that todays value is higher than yesterdays.
@@ -35,33 +37,17 @@ TemporalRelativeDifference=function(Data,Lag=1,na.rm=FALSE,PlotIt=FALSE,Time,Sil
       na.rm = na.rm,
       Silent=Silent
     )
+    if(!missing(Time)){
+      rownames(Rel)=Time
+    }else if(!is.null(rownames(Data))){
+      rownames(Rel)= rownames(Data)
+    }else{
+      #do noting
+    }
+    
     return(Rel)
   } else{#Data is vector
-    neg = which(Data < 0)
-    if (length(neg) > 0) {
-      Data[neg] = 0
-      if(isFALSE(Silent))
-        warning(
-          "TemporalRelativeDifference works only for positive values, negatives are set to zero per default which my be incorrect."
-        )
-    }
-    nan_ind = which(!is.finite(Data))
     
-    if (length(nan_ind) > 0) {
-      if (isTRUE(na.rm)){
-          Data=imputeTS::na.interpolation(Data,option = 'linear')
-      }
-        if(isFALSE(Silent))
-          message(
-            "TemporalRelativeDifference works only for positive values correctly but missing values were found. These cases are linearly interpolated."
-          )
-      }else{
-        if(isFALSE(Silent))
-          warning(
-            "TemporalRelativeDifference works only for positive values correctly but missing values were found."
-          )
-      }
-  
     neg_ind = which(Data<0)
     if (length(neg_ind) > 0) {
       if (isTRUE(na.rm)){
@@ -77,9 +63,24 @@ TemporalRelativeDifference=function(Data,Lag=1,na.rm=FALSE,PlotIt=FALSE,Time,Sil
           "TemporalRelativeDifference works only for positive values correctly but negative values were found."
         )
     }
+    nan_ind = which(!is.finite(Data))
     
-    Today = Data
+    if (length(nan_ind) > 0) {
+      if (isTRUE(na.rm)){
+          Data=imputeTS::na_interpolation(Data,option = 'linear')
+      }
+        if(isFALSE(Silent))
+          message(
+            "TemporalRelativeDifference works only for positive values correctly but missing values were found. These cases are linearly interpolated."
+          )
+      }else{
+        if(isFALSE(Silent))
+          warning(
+            "TemporalRelativeDifference works only for positive values correctly but missing values were found."
+          )
+      }
   
+    Today = Data
     Yesterday = LagVector(Data, k = Lag)
     
     #no nan
@@ -89,6 +90,12 @@ TemporalRelativeDifference=function(Data,Lag=1,na.rm=FALSE,PlotIt=FALSE,Time,Sil
     if (Lag < 0) {
       indNan = tail(1:length(Yesterday), Lag)
       Yesterday[indNan] = Yesterday[min(indNan) - 1]
+      
+      warning("TemporalRelativeDifference: Under development for lag <0")
+      #you have to swith today with yesterday
+      tmp=Today
+      Today=Yesterday #yesterday variable is tomorrow!
+      Yesterday=tmp #todays variable is yesterday
     }
     
     if (packageVersion("DatabionicSwarm") != "1.3.0") {
@@ -99,6 +106,14 @@ TemporalRelativeDifference=function(Data,Lag=1,na.rm=FALSE,PlotIt=FALSE,Time,Sil
       Rel = DatabionicSwarm::RelativeDifference(Y = Today, X = Yesterday, 
                                                 na.rm = F,Silent=Silent)
     }
+    if(!missing(Time)){
+      names(Rel)=Time
+    }else if(!is.null(names(Today))){
+        names(Rel)= names(Today)
+    }else{
+      #do noting
+    }
+   
     if (isTRUE(na.rm))
       Rel[!is.finite(Rel)] = 0
     
